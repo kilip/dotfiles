12c853cf4c53f06198d271e2d1e60844f54de446
Anthonius Munthi <me@itstoni.com>
2024-08-05 16:08:18 +0000

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,6 +3,6 @@ if status is-interactive
 end
 
 
-if type -q /home/linuxbrew/.linuxbrew/bin/brew
+if test -f /home/linuxbrew/.linuxbrew/bin/brew
   eval "/home/linuxbrew/.linuxbrew/bin/brew shellenv"
 end
