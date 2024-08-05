1e31b792bb442a67a196e29a9832ac524ce1faae
Anthonius Munthi <me@itstoni.com>
2024-08-05 14:49:15 +0000

    initial commit :rocket:

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e # -e: exit on error
+
+# install age key
+mkdir -p "$HOME/.config/sops/age"
+pass age/keys >> "$HOME/.config/sops/age/keys.txt"
+
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+fi
