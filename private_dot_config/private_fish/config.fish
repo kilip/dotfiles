757f617a3a9f6c7462b03e64d758e132330b1009
Anthonius Munthi <me@itstoni.com>
2024-08-06 13:41:12 +0800

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,13 +4,13 @@ end
 
 
 if test -f /home/linuxbrew/.linuxbrew/bin/brew
-  eval "/home/linuxbrew/.linuxbrew/bin/brew shellenv"
+  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
-  #if test -d (brew --prefix)"/share/fish/completions"
-  #  set -p fish_complete_path (brew --prefix)/share/fish/completions
-  #end
+  if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+  end
 
-  #if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-  #  set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-  #end
+  if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+  end
 end
